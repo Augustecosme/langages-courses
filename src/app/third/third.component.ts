454c4141bb15a12a9dee36cf1e41511f2d2a9fea
Auguste QUENUM <auguste.quenum@chem.loc>
2024-12-20 13:00:20 +0100

    first commit

---
+++ src/app/third/third.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-third',
+  standalone: false,
+  
+  templateUrl: './third.component.html',
+  styleUrl: './third.component.css'
+})
+export class ThirdComponent {
+
+}
